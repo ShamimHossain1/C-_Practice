28f9d2b4e400b758ee2e79bc2471b4782d3b6031
ShamimHossain1 <shamimweb78@gmail.com>
2024-04-29 22:41:46 -0400

    count'

---
+++ 1To_1b_count.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    long long int count = 1;
+
+    while (count < 1000000000) {
+       
+        count++;
+    }
+     cout << count << endl;
+
+    return 0;
+}
---
+++ count1_to_1b.js
@@ -0,0 +1,7 @@
+let count = 1;
+
+while (count <= 1000000000) {
+  
+    count++;
+}
+console.log(count);

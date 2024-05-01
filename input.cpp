7850b7c81aa5116ec0a8fb53bc852ccba3688ae8
ShamimHossain1 <shamimweb78@gmail.com>
2024-04-30 21:33:46 -0400

    operators

---
+++ input.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num1, num2;
+    cout<<"Enter number 1: ";
+    cin>>num1;
+      
+    cout<<"Enter number 2: ";
+    cin>>num2;
+
+    int sum = (num1 + num2);
+    cout<<"Sum of two numbers is:"<<sum;
+
+    /* code */
+    return 0;
+}
---
+++ operators.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a = 5, b = 4;
+
+    cout << "---Operators in C++---" << endl<< endl;
+
+    cout << "The value of a+b is : " << a + b << endl;
+    cout << "The value of a-b is : " << a - b << endl;
+    cout << "The value of a+b is : " << a + b << endl;
+    cout << "The value of a*b is : " << a * b << endl;
+    cout << "The value of a/b is : " << a / b << endl;
+    cout << "The value of a%b is : " << b % a << endl;
+    cout << "The value of a++ is : " << a++ << endl;
+    cout << "The value of a-- is : " << a-- << endl;
+    cout << "The value of ++a is : " << ++a << endl;
+    cout << "The value of --a is : " << --a << endl<<endl;
+
+    // Logical operators
+
+    cout<< "---Logical operators---"<<endl<<endl;
+
+    cout << "The value of a==b is : " << (a == b) << endl;
+    cout << "The value of (a!=b) is : " << (a != b) << endl;
+    cout << "The value of (a<b) is : " << (a < b) << endl;
+    cout << "The value of (a>b) is : " << (a > b) << endl<<endl;
+
+    cout << "The value of ((a != b) && (a < b)) is : " << ((a != b) && (a < b)) << endl;
+    cout << "The value of ((a > b) || (a == b)) is : " << ((a > b) || (a == b)) << endl;
+
+    return 0;
+}
